defcc3a9a609a1880b94dfd5e379cccd68b387df
Robert Fowler <robertf@ipay.co.za>
2018-08-25 17:29:19 +0200

    Add bootstrap for data.

--- pet-clinic-data/src/main/java/guru/springframework/sftpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sftpetclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package guru.springframework.sftpetclinic.services.map;
 
 import guru.springframework.sftpetclinic.model.Owner;
-import guru.springframework.sftpetclinic.services.CrudService;
+import guru.springframework.sftpetclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements OwnerService {
 
     @Override
     public Owner findById(Long id) {
@@ -30,5 +30,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements C
     @Override
     public void deleteById(Long id) {
         super.deleteById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
