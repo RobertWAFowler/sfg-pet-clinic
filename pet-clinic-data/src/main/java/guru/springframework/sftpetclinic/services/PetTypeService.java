fa5b2beb0a8ff2ebbb33537a966b9eeb3d0a76d9
Robert Fowler <robertf@ipay.co.za>
2018-08-30 21:09:52 +0200

    Create PetType Map Service Impl, Closes #30

---
+++ pet-clinic-data/src/main/java/guru/springframework/sftpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sftpetclinic.services;
+
+import guru.springframework.sftpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
