8913f63284015ab065bbd822652d743b1f49dd96
RobertWAFowler <rfowler.reg@gmail.com>
2018-12-08 16:00:57 +0200

    219. Spring Pet Clinic - Display Owner Record. Closes #54

--- pet-clinic-web/src/main/java/guru/springframework/sftpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sftpetclinic/controllers/OwnersController.java
@@ -3,8 +3,10 @@ package guru.springframework.sftpetclinic.controllers;
 import guru.springframework.sftpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
@@ -26,5 +28,12 @@ public class OwnersController {
     @RequestMapping("/find")
     public String findOwners(){
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
